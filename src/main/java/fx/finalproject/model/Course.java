1ea6036a5c8cf166e2ed616e6c7d0dcba72d0177
AbdUllah0x52 <3bdallara2fata.r.m@gmail.com>
2024-12-21 19:22:53 +0200

    Course control (#3)
    * Init
    
    * Add authentication feature
    
    * Add course control

--- src/main/java/fx/finalproject/model/Course.java
+++ src/main/java/fx/finalproject/model/Course.java
@@ -1,4 +1,22 @@
 package fx.finalproject.model;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Course {
+    private final SimpleStringProperty courseId;
+    private final SimpleStringProperty  courseName;
+
+    public Course(String courseId, String courseName) {
+        this.courseId = new SimpleStringProperty(courseId);
+        this.courseName = new SimpleStringProperty(courseName);
+    }
+    public String getCourseId() {
+        return courseId.getValue();
+    }
+    public String getCourseName() {
+        return courseName.getValue();
+    }
+    public void setCourseName(String courseName) {
+        this.courseName.setValue(courseName);
+    }
 }
