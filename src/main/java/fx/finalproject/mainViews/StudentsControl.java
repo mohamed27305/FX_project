4598f37169e3544c1831afe70ce0384179494230
mohamed27305 <154395792+mohamed27305@users.noreply.github.com>
2024-12-22 13:51:16 +0200

    error commit

--- src/main/java/fx/finalproject/mainViews/StudentsControl.java
+++ src/main/java/fx/finalproject/mainViews/StudentsControl.java
@@ -1,22 +1,43 @@
 package fx.finalproject.mainViews;
 
+import fx.finalproject.DataBase;
 import fx.finalproject.Navigator;
 import fx.finalproject.interfaces.UIClass;
+import fx.finalproject.model.Course;
+import fx.finalproject.model.Student;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Bounds;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class StudentsControl implements UIClass {
     private BorderPane root;
     private final Navigator navigator;
-    public StudentsControl(Navigator navigator){
+    TextField newstudentId;
+    TextField newstudentName;
+    TextField studentId;
+    TextField addcourse;
+    TextField removecourse;
+    TableView<Student> studentTable;
+    ObservableList<Student> data = FXCollections.observableArrayList();
+    Alert alert = new Alert(Alert.AlertType.ERROR);
+
+    public StudentsControl(Navigator navigator) {
         this.navigator = navigator;
         setRoot();
+        setData();
 
     }
+
     private void setRoot() {
         root = new BorderPane();
         Label title = new Label("Students Control Panel");
@@ -30,14 +51,40 @@ public class StudentsControl implements UIClass {
         body.setVgap(20);
         body.setAlignment(Pos.CENTER);
 
+        Button addStudent = new Button("Add new Student");
+        addStudent.setOnAction((var) -> addStudentAction());
+        newstudentId = new TextField();
+        newstudentId.setPromptText("student id");
+        newstudentName = new TextField();
+        newstudentName.setPromptText("student name");
+        body.add(addStudent, 0, 0);
+        body.add(newstudentId, 1, 0);
+        body.add(newstudentName, 2, 0);
+
+
         Button back = new Button("Back");
         VBox footer = new VBox(back);
         footer.setStyle("-fx-padding:10;");
         footer.setAlignment(Pos.BOTTOM_RIGHT);
 
+        studentTable = new TableView<>();
+        TableColumn<Student, String> namecolumn = new TableColumn<>("student name");
+        namecolumn.setCellValueFactory(new PropertyValueFactory<>("studentName"));
+        TableColumn<Student, String> idcolumn = new TableColumn<>("student id");
+        idcolumn.setCellValueFactory(new PropertyValueFactory<>("studentId"));
+        studentTable.getColumns().add(namecolumn);
+        studentTable.getColumns().add(idcolumn);
+        studentTable.setItems(data);
+
+
+        VBox right = new VBox(studentTable);
+        right.setSpacing(10);
+        right.setAlignment(Pos.TOP_CENTER);
+
         root.setTop(header);
         root.setCenter(body);
         root.setBottom(footer);
+        root.setRight(right);
 
         back.setOnAction((var) -> backAction());
     }
@@ -45,6 +92,61 @@ public class StudentsControl implements UIClass {
     @Override
     public BorderPane getRoot() {
         return root;
+    }
+
+    private void addStudentAction() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        String studentId = newstudentId.getText();
+        String studentName = newstudentName.getText();
+
+        if (studentId.isBlank() || studentId.isEmpty()) {
+            alert.setContentText("student id is required!");
+            alert.show();
+            return;
+        }
+        if (studentName.isBlank() || studentName.isEmpty()) {
+            alert.setContentText("student name is required!");
+            alert.show();
+            return;
+        }
+
+        try {
+            Connection con = DataBase.getConnect();
+            String query = String.format("INSERT INTO Student VALUES ('%s','%s')", studentId, studentName);
+            assert con != null;
+            Statement stmt = con.createStatement();
+            stmt.executeQuery(query);
+            con.close();
+            alert.setAlertType(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setContentText("Added new student!");
+            alert.show();
+            newstudentId.setText("");
+            newstudentName.setText("");
+            data.add(new Student(studentId, studentName));
+        } catch (Exception e) {
+            System.out.println("[-] Error " + e);
+            alert.setContentText("Error Can't add new student!");
+            alert.show();
+        }
+    }
+
+    private void setData() {
+        data.clear();
+        try {
+            Connection con = DataBase.getConnect();
+            String query = "SELECT * FROM Student";
+            assert con != null;
+            Statement stmt = con.createStatement();
+            ResultSet resultSet = stmt.executeQuery(query);
+            while (resultSet.next()) {
+                data.add(new Student(resultSet.getString(1), resultSet.getString(2)));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Error " + e);
+        }
     }
 
     private void backAction() {
--- src/main/java/fx/finalproject/model/Student.java
+++ src/main/java/fx/finalproject/model/Student.java
@@ -1,4 +1,20 @@
 package fx.finalproject.model;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Student {
+    private final SimpleStringProperty studentId;
+    private final SimpleStringProperty studentName;
+
+    public Student(String studentId, String studentName) {
+        this.studentId = new SimpleStringProperty(studentId);
+        this.studentName = new SimpleStringProperty(studentName);
+    }
+   public String getStudentId(){
+        return studentId.getValue();
+   }
+   public void setStudentName(String studentName){
+        this.studentName.setValue(studentName);
+   }
+
 }
