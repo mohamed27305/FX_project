323c21e75bcd726ad6d5a298bfb4f6bb6ee5fced
mohamed27305 <154395792+mohamed27305@users.noreply.github.com>
2024-12-22 20:35:49 +0200

    finish professor page

--- src/main/java/fx/finalproject/mainViews/ProfessorsControl.java
+++ src/main/java/fx/finalproject/mainViews/ProfessorsControl.java
@@ -1,17 +1,36 @@
 package fx.finalproject.mainViews;
 
+import fx.finalproject.DataBase;
 import fx.finalproject.Navigator;
 import fx.finalproject.interfaces.UIClass;
+import fx.finalproject.model.Course;
+import fx.finalproject.model.Professor;
+import fx.finalproject.model.Student;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class ProfessorsControl implements UIClass {
     private final Navigator navigator;
     private BorderPane root;
+    TextField newprofId;
+    TextField newprofName;
+    TextField profId;
+    TextField courseId;
+    TextField removecourse;
+    TableView<Professor> profTable;
+    ObservableList<Professor> data = FXCollections.observableArrayList();
+    TableView<Course> courseTable;
+    ObservableList<Course> courses = FXCollections.observableArrayList();
+    Alert alert = new Alert(Alert.AlertType.ERROR);
 
     public ProfessorsControl(Navigator navigator) {
         this.navigator = navigator;
@@ -26,23 +45,133 @@ public class ProfessorsControl implements UIClass {
         header.setStyle("-fx-background-color:blue;");
         header.setAlignment(Pos.CENTER);
 
+
         GridPane body = new GridPane();
         body.setHgap(20);
         body.setVgap(20);
         body.setAlignment(Pos.CENTER);
+
+        Button addProfessor = new Button("Add new professor");
+        addProfessor.setOnAction((var) -> addProfessorAction());
+        newprofId = new TextField();
+        newprofId.setPromptText("professor id");
+        newprofName = new TextField();
+        newprofName.setPromptText("professor name");
+        body.add(addProfessor, 0, 0);
+        body.add(newprofId, 1, 0);
+        body.add(newprofName, 2, 0);
+
+        Button addCourse = new Button("add course");
+        addCourse.setOnAction((var)-> addCourse());
+        courseId = new TextField();
+        profId = new TextField();
+        courseId.setPromptText("course Id");
+        profId.setPromptText("professor Id");
+        body.add(addCourse,0,1);
+        body.add(profId,1,1);
+        body.add(courseId,2,1);
+
+
 
         Button back = new Button("Back");
         VBox footer = new VBox(back);
         footer.setStyle("-fx-padding:10;");
         footer.setAlignment(Pos.BOTTOM_RIGHT);
 
+        profTable = new TableView<>();
+        TableColumn<Professor, String> namecolumn = new TableColumn<>("professor name");
+        namecolumn.setCellValueFactory(new PropertyValueFactory<>("profName"));
+        TableColumn<Professor, String> idcolumn = new TableColumn<>("professor id");
+        idcolumn.setCellValueFactory(new PropertyValueFactory<>("profId"));
+        profTable.getColumns().add(namecolumn);
+        profTable.getColumns().add(idcolumn);
+        profTable.setItems(data);
+
+        VBox right = new VBox(profTable);
+        right.setSpacing(10);
+        right.setAlignment(Pos.TOP_CENTER);
+
         root.setTop(header);
         root.setCenter(body);
         root.setBottom(footer);
+        root.setRight(right);
 
         back.setOnAction((var) -> backAction());
     }
+    private void addProfessorAction() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        String profId = newprofId.getText();
+        String profName = newprofName.getText();
 
+        if (profId.isBlank() || profId.isEmpty()) {
+            alert.setContentText("student id is required!");
+            alert.show();
+            return;
+        }
+        if (profName.isBlank() || profName.isEmpty()) {
+            alert.setContentText("student name is required!");
+            alert.show();
+            return;
+        }
+
+        try {
+            Connection con = DataBase.getConnect();
+            String query = String.format("INSERT INTO PROFESSOR VALUES ('%s','%s')", profId, profName);
+            assert con != null;
+            Statement stmt = con.createStatement();
+            stmt.executeQuery(query);
+            con.close();
+            alert.setAlertType(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setContentText("Added new professor!");
+            alert.show();
+            newprofId.setText("");
+            newprofName.setText("");
+            data.add(new Professor(profId, profName));
+        } catch (Exception e) {
+            System.out.println("[-] Error " + e);
+            alert.setContentText("Error Can't add new professor!");
+            alert.show();
+        }
+    }
+    private void addCourse(){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        String profId = this.profId.getText();
+        String courseId = this.courseId.getText();
+
+        if (profId.isBlank() || profId.isEmpty()){
+            alert.setContentText("professor id is required!");
+            alert.show();
+            return;
+        }
+        if (courseId.isBlank() || courseId.isEmpty()){
+            alert.setContentText("Course id is required!");
+            alert.show();
+            return;
+        }
+
+        try {
+            Connection con = DataBase.getConnect();
+            String query = String.format("INSERT INTO TEACHING VALUES ('%s','%s')",profId,courseId);
+            assert con != null;
+            Statement stmt= con.createStatement();
+            stmt.executeQuery(query);
+            con.close();
+            alert.setAlertType(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setContentText("Added new course to professor!");
+            alert.show();
+            this.profId.setText("");
+            this.courseId.setText("");
+        }
+        catch (Exception e) {
+            System.out.println("[-] Error "+e);
+            alert.setContentText("Error Can't add new course to professor!");
+            alert.show();
+        }
+    }
     @Override
     public BorderPane getRoot() {
         return root;
--- src/main/java/fx/finalproject/model/Professor.java
+++ src/main/java/fx/finalproject/model/Professor.java
@@ -1,4 +1,24 @@
 package fx.finalproject.model;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Professor {
+    private final SimpleStringProperty profId;
+    private final SimpleStringProperty profName;
+
+    public Professor(String profId, String profName) {
+        this.profId = new SimpleStringProperty(profId);
+        this.profName = new SimpleStringProperty(profName);
+    }
+    public String getProfId(){
+        return profId.getValue();
+    }
+    public void setProfName(String profName){
+        this.profName.setValue(profName);
+    }
+
+    public String getProfName() {
+        return profName.getValue();
+    }
+
 }
